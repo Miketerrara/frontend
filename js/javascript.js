c64ac59e5e77ca77226bc7b25e9f4239fce26413
Miketerrara <michael.designer@protonmail.com>
2025-01-15 14:03:14 -0300

    tela aguardem completa

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,9 @@
 const _elements = {
-    toggle: document.querySelector(".bars"),
-    navbar: document.querySelector(".bars__nav"),
-    navbarClose: document.querySelector(".fa-xmark"),
-    navbarLinks: document.querySelectorAll(".bars__link"),
+    bar:  document.querySelector('.bar'),
+    barScreen: document.querySelector('.bar-modal'),
+    barLinks: document.querySelectorAll('.item'),
 }
 
-function linkNavbar() {
-	_elements.navbarLinks.forEach(link => {
-		link.classList.toggle("id--toggle");
-	});
-
-    _elements.navbar.classList.toggle("bars__nav--active");
-}
-
-_elements.toggle.addEventListener("click", linkNavbar);
-
-_elements.navbarClose.addEventListener("click", () =>{
-    _elements.toggle.dispatchEvent(new Event("click"));
+_elements.bar.addEventListener('click', () =>{
+    _elements.barScreen.classList.toggle('bar-modal--active');
 });
